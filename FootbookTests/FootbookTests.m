35ebf7aec6fdd22f19e0dbf5de34bf1347572a83
Steve Toosevich <stoosevich@gmail.com>
2014-04-02 14:05:19 -0500

    Initial Commit

---
+++ FootbookTests/FootbookTests.m
@@ -0,0 +1,34 @@
+//
+//  FootbookTests.m
+//  FootbookTests
+//
+//  Created by Steve Toosevich on 4/2/14.
+//  Copyright (c) 2014 Steve Toosevich. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FootbookTests : XCTestCase
+
+@end
+
+@implementation FootbookTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
